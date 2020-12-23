d6a1e84fdd2d4ea49b4132b988d53c20a0277eae
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2020-12-23 14:33:16 +0600

    prepare for deployment

---
+++ shared/constants.js
@@ -0,0 +1,7 @@
+exports.DBConstants = {
+    MongoDbUri: 'mongodb+srv://abmfahimshahriar:154410north@cluster0-xe9ja.mongodb.net/tech_shop',
+}
+exports.KeyConstants = {
+    AdminTokenKey: 'somesupersecretforadmin',
+    AuthTokenKey: 'somesupersecretsecret',
+}
